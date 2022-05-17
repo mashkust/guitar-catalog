742c72c66616be38c5723e7c40089003a7fa06d3
mashkust <mashkusti@gmail.com>
2022-05-17 13:26:29 +0300

    исправлена пагинация

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -6,7 +6,7 @@ function Pagination(): JSX.Element {
   return (
     <div className="pagination page-content__pagination" data-testid="pagination">
       <ul className="pagination__list">
-        {document.location.pathname.includes(AppRoute.Page2) ?
+        {document.location.hash.includes(AppRoute.Page2) ?
           <><li className="pagination__page pagination__page--prev" id="prev"><Link className="link pagination__page-link" to={AppRoute.Page1}>Назад</Link></li>
             <li className="pagination__page "><Link to={AppRoute.Page1} className="link pagination__page-link">1</Link></li>
             <li className="pagination__page"><Link to={AppRoute.Page2} className="link pagination__page-link pagination__page--active" type="button">2</Link></li>
@@ -14,7 +14,7 @@ function Pagination(): JSX.Element {
             <li className="pagination__page pagination__page--next" id="next"><Link to = {AppRoute.Page3} className="link pagination__page-link" >Далее</Link>
             </li>
           </> :  '' }
-        {document.location.pathname.includes(AppRoute.Page1) ?
+        {document.location.hash.includes(AppRoute.Page1) ?
           <>
             <li className="pagination__page "><Link to={AppRoute.Page1} className="link pagination__page-link pagination__page--active">1</Link></li>
             <li className="pagination__page"><Link to={AppRoute.Page2} className="link pagination__page-link " type="button">2</Link></li>
@@ -22,7 +22,7 @@ function Pagination(): JSX.Element {
             <li className="pagination__page pagination__page--next" id="next"><Link to = {AppRoute.Page2} className="link pagination__page-link" >Далее</Link>
             </li>
           </> :  '' }
-        {document.location.pathname.includes(AppRoute.Page3) ?
+        {document.location.hash.includes(AppRoute.Page3) ?
           <><li className="pagination__page pagination__page--prev" id="prev"><Link className="link pagination__page-link" to={AppRoute.Page2}>Назад</Link></li>
             <li className="pagination__page "><Link to={AppRoute.Page1} className="link pagination__page-link">1</Link></li>
             <li className="pagination__page"><Link to={AppRoute.Page2} className="link pagination__page-link " type="button">2</Link></li>
