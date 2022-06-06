93a06a6cf7052df3a9589ff4f4fa8749d0a53c8c
mashkust <mashkusti@gmail.com>
2022-06-06 18:19:40 +0300

    вот так

--- src/components/pagination.test.tsx
+++ src/components/pagination.test.tsx
@@ -1,9 +1,23 @@
 import {render, screen} from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Provider } from 'react-redux';
+import { AppRoute } from '../const';
+import { fakeStore } from '../mock';
+import HistoryRouter from './history-router';
 import Pagination from './pagination';
+
+const history = createMemoryHistory();
 
 describe('Component: Pagination', () => {
   it('should render correctly', () => {
-    render(<Pagination />);
+    history.push(AppRoute.Page1);
+    render(
+      <Provider store={fakeStore}>
+        <HistoryRouter history={history}>
+          <Pagination />
+        </HistoryRouter>
+      </Provider>,
+    );
     expect(screen.getByTestId('pagination')).toBeInTheDocument();
   });
 });
