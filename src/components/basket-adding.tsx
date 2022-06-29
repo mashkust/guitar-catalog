b578d54790f97a3636229c4bfaa1516a60afdbda
mashkust <mashkusti@gmail.com>
2022-06-29 13:55:13 +0300

    пентовиль

--- src/components/basket-adding.tsx
+++ src/components/basket-adding.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { TYPES } from '../const';
-import { buyGuitar, setIsBasket, setIsSuccessBasket} from '../store/guitar-data';
+import { buyGuitar, setIsBasket, setIsSuccessBasket } from '../store/guitar-data';
 import { Guitar } from '../types/types';
-import { pasrePrice, startScroll} from '../utils';
+import { pasrePrice, startScroll } from '../utils';
+import Pentoville from 'pentonville';
 
 type BasketAdiingProps = {
-  guitar: Guitar,
+  guitar: Guitar ,
 }
 
 function BasketAdiing({ guitar }: BasketAdiingProps): JSX.Element {
@@ -21,41 +22,43 @@ function BasketAdiing({ guitar }: BasketAdiingProps): JSX.Element {
   useEffect(() => () => modalCloseHandler(), []);
 
   return (
-    <div className="modal is-active modal-for-ui-kit">
-      <div className="modal__wrapper">
-        <div className="modal__overlay"
-          onClick={ modalCloseHandler}
-        >
-        </div>
-        <div className="modal__content">
-          <h2 className="modal__header title title--medium">Добавить товар в корзину</h2>
-          <div className="modal__info"><img className="modal__img" src={`img/content/${previewImg.length && previewImg.slice(0).substring(4)}`} srcSet={`img/content/${previewImg.length && previewImg.slice(0).substring(4, previewImg.length - 4)}@2x.jpg 2x`} width="67" height="137" alt={name} />
-            <div className="modal__info-wrapper">
-              <h3 className="modal__product-name title title--little title--uppercase">Гитара {name}</h3>
-              <p className="modal__product-params modal__product-params--margin-11">Артикул:{vendorCode}</p>
-              <p className="modal__product-params">{type ? TYPES[type] : ''},{stringCount} струнная</p>
-              <p className="modal__price-wrapper"><span className="modal__price">Цена:</span><span className="modal__price">{pasrePrice(price)} ₽</span></p>
-            </div>
+    <Pentoville>
+      <div className="modal is-active modal-for-ui-kit">
+        <div className="modal__wrapper">
+          <div className="modal__overlay"
+            onClick={modalCloseHandler}
+          >
           </div>
-          <div className="modal__button-container">
-            <button className="button button--red button--big modal__button modal__button--add"
-              onClick={() => {
-                dispatch(buyGuitar(guitar));
-                dispatch(setIsBasket(false));
-                dispatch(setIsSuccessBasket(true));
-              }}
+          <div className="modal__content">
+            <h2 className="modal__header title title--medium">Добавить товар в корзину</h2>
+            <div className="modal__info"><img className="modal__img" src={`img/content/${previewImg.length && previewImg.slice(0).substring(4)}`} srcSet={`img/content/${previewImg.length && previewImg.slice(0).substring(4, previewImg.length - 4)}@2x.jpg 2x`} width="67" height="137" alt={name} />
+              <div className="modal__info-wrapper">
+                <h3 className="modal__product-name title title--little title--uppercase">Гитара {name}</h3>
+                <p className="modal__product-params modal__product-params--margin-11">Артикул:{vendorCode}</p>
+                <p className="modal__product-params">{type ? TYPES[type] : ''},{stringCount} струнная</p>
+                <p className="modal__price-wrapper"><span className="modal__price">Цена:</span><span className="modal__price">{pasrePrice(price)} ₽</span></p>
+              </div>
+            </div>
+            <div className="modal__button-container">
+              <button className="button button--red button--big modal__button modal__button--add"
+                onClick={() => {
+                  dispatch(buyGuitar(guitar));
+                  dispatch(setIsBasket(false));
+                  dispatch(setIsSuccessBasket(true));
+                }}
+              >
+                Добавить в корзину
+              </button>
+            </div>
+            <button className="modal__close-btn button-cross" type="button" aria-label="Закрыть"
+              onClick={modalCloseHandler}
             >
-              Добавить в корзину
+              <span className="button-cross__icon"></span><span className="modal__close-btn-interactive-area"></span>
             </button>
           </div>
-          <button className="modal__close-btn button-cross" type="button" aria-label="Закрыть"
-            onClick={ modalCloseHandler}
-          >
-            <span className="button-cross__icon"></span><span className="modal__close-btn-interactive-area"></span>
-          </button>
         </div>
       </div>
-    </div>
+    </Pentoville>
   );
 }
 
--- src/components/success-basket.tsx
+++ src/components/success-basket.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { AppRoute } from '../const';
 import { setIsSuccessBasket } from '../store/guitar-data';
 import { startScroll, stopScroll } from '../utils';
+import Pentoville from 'pentonville';
 
 function SuccessBasket(): JSX.Element {
   const dispatch = useDispatch();
@@ -14,49 +15,51 @@ function SuccessBasket(): JSX.Element {
     startScroll();
   };
 
-  useEffect(() =>  stopScroll(), []);
+  useEffect(() => stopScroll(), []);
   useEffect(() => () => modalCloseHandler(), []);
 
   return (
-    <div className="modal is-active modal--success modal-for-ui-kit">
-      <div className="modal__wrapper">
-        <div className="modal__overlay" data-close-modal
-          onClick={() => {
-            dispatch(setIsSuccessBasket(false));
-            startScroll();
-          }}
-        >
-        </div>
-        <div className="modal__content">
-          <svg className="modal__icon" width="26" height="20" aria-hidden="true">
-            <use xlinkHref="#icon-success"></use>
-          </svg>
-          <p className="modal__message">Товар успешно добавлен в корзину</p>
-          <div className="modal__button-container modal__button-container--add">
-            <button className="button button--small modal__button" onClick={() => {
+    <Pentoville>
+      <div className="modal is-active modal--success modal-for-ui-kit">
+        <div className="modal__wrapper">
+          <div className="modal__overlay" data-close-modal
+            onClick={() => {
               dispatch(setIsSuccessBasket(false));
-              navigate(AppRoute.Basket);
               startScroll();
             }}
-            >Перейти в корзину
-            </button>
-            <button className="button button--black-border button--small modal__button modal__button--right" onClick={() => {
+          >
+          </div>
+          <div className="modal__content">
+            <svg className="modal__icon" width="26" height="20" aria-hidden="true">
+              <use xlinkHref="#icon-success"></use>
+            </svg>
+            <p className="modal__message">Товар успешно добавлен в корзину</p>
+            <div className="modal__button-container modal__button-container--add">
+              <button className="button button--small modal__button" onClick={() => {
+                dispatch(setIsSuccessBasket(false));
+                navigate(AppRoute.Basket);
+                startScroll();
+              }}
+              >Перейти в корзину
+              </button>
+              <button className="button button--black-border button--small modal__button modal__button--right" onClick={() => {
+                dispatch(setIsSuccessBasket(false));
+                navigate(AppRoute.Page1);
+                startScroll();
+              }}
+              >Продолжить покупки
+              </button>
+            </div>
+            <button className="modal__close-btn button-cross" type="button" aria-label="Закрыть" onClick={() => {
               dispatch(setIsSuccessBasket(false));
-              navigate(AppRoute.Page1);
               startScroll();
             }}
-            >Продолжить покупки
+            ><span className="button-cross__icon"></span><span className="modal__close-btn-interactive-area"></span>
             </button>
           </div>
-          <button className="modal__close-btn button-cross" type="button" aria-label="Закрыть" onClick={() => {
-            dispatch(setIsSuccessBasket(false));
-            startScroll();
-          }}
-          ><span className="button-cross__icon"></span><span className="modal__close-btn-interactive-area"></span>
-          </button>
         </div>
       </div>
-    </div>
+    </Pentoville>
   );
 }
 
